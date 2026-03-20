594683bf6b0fe4496a1398865fe9b41c0955edc8
Bart Veneman <bartveneman@gmail.com>
2026-03-20 09:08:01 +0100

    lint

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "tsdown";
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: "./src/index.ts",
-  format: ["esm"],
-  platform: "node",
-  dts: true,
-  publint: true,
-});
+	entry: './src/index.ts',
+	format: ['esm'],
+	platform: 'node',
+	dts: true,
+	publint: true,
+})
