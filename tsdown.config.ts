a41250e316975c1d77d4543176bcfbfcb707d456
Bart Veneman <bartveneman@gmail.com>
2026-03-20 09:07:06 +0100

    chore: implement publint

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
-	entry: './src/index.ts',
-	format: ['esm'],
-	platform: 'node',
-	dts: true,
-})
+  entry: "./src/index.ts",
+  format: ["esm"],
+  platform: "node",
+  dts: true,
+  publint: true,
+});
